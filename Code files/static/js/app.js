5a921207c0015d9a55178014c33d842016392411
scmo-ops <78674164+scmo-ops@users.noreply.github.com>
2021-07-03 20:53:55 -0500

    try old code

--- Code files/static/js/app.js
+++ Code files/static/js/app.js
@@ -1,18 +1,64 @@
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata= data.metadata;
+      var resultsarray= metadata.filter(sampleobject => 
+        sampleobject.id == sample);
+      var result= resultsarray[0]
+      var panel = d3.select("#sample-metadata");
+      panel.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        panel.append("h6").text(`${key}: ${value}`);
+      });
+  
+    //buildGauge(result.wfreq)
+  
+  
+  
+    });
+  }
+
 
 // this section reads the data from the json file
 
-function init(){
-    d3.json("data/samples.json").then((data) => {
-
-        //  Create the Traces
-
-        var trace1 = {
-            // Values for the bar chart
-            x: samples.otu_ids,
-            y: samples.sample_values,
-            text: samples.otu_labels,
-            type: 'bar',
-            name: 'top 10 OTUs',
-            orientation: 'h'
-        };
-}
+d3.json("data/data.json").then((importedata) => {
+    // console.log(importedData);
+    var data = importedata;
+  
+    data.sort(function(a, b) {
+      return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+    });
+  
+    // Slice the first 10 objects for plotting
+    data = data.slice(0, 10);
+  
+    // Reverse the array due to Plotly's defaults
+    data = data.reverse();
+  
+    // Trace1 for the Greek Data
+    var trace1 = {
+      x: data.map(row => row.greekSearchResults),
+      y: data.map(row => row.greekName),
+      text: data.map(row => row.greekName),
+      name: "Greek",
+      type: "bar",
+      orientation: "h"
+    };
+  
+    // data
+    var chartData = [trace1];
+  
+    // Apply the group bar mode to the layout
+    var layout = {
+      title: "Greek gods search results",
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+      }
+    };
+  
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("plot", chartData, layout);
+  });
+  
