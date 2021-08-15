afe0f51c35b654bd1bdd532778a50ebde85e9d96
scmo-ops <a01610125@exatec.tec.mx>
2021-08-15 16:28:30 -0500

    Time to debug

--- static/app.js
+++ static/app.js
@@ -27,7 +27,7 @@ function charts(id) {
 
     // Connect to the json file.
 
-    d3.json("samples.json").then((data)=> {
+    d3.json("data/samples.json").then((data)=> {
         
         //filter frecuencies value by id
         var frecuencies = data.metadata.filter(f => f.id.toString() === id)[0];
@@ -67,7 +67,88 @@ function charts(id) {
         ////// Section for the charts //////
   
   
-        // Bar chats
+        // Bar chat
+
+        //// I decided to use chartsly since it proves to be very easy to use and very effective ////
+
         chartsly.newcharts("bar", data);
       
-        
+        // The bubble chart
+        var New_trace = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+  
+        };
+  
+        var bubble_layout = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1200
+        };
+  
+        var buble_data = [New_trace];
+  
+        chartsly.newcharts("bubble", buble_data, bubble_layout); 
+  
+        // Guage chart
+        var Guage_data = [
+          {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: frecuencies,
+          title: {text: `Belly Button Washing Frequency`},
+          type: "indicator",
+          
+          mode: "gauge+number",
+          gauge: { axis: { range: [null, 9] },
+                   steps: [
+                    {range: [0, 1], color: "white"},
+                    {range: [1, 2], color: "white"},
+                    {range: [2, 3], color: "white"},
+                    {range: [3, 4], color: "white"},
+                    {range: [4, 5], color: "white"},
+                    {range: [5, 6], color: "white"},
+                    {range: [6, 7], color: "white"},
+                    {range: [7, 8], color: "white"},
+                    {range: [8, 9], color: "white"}
+                  ]}
+              
+          }
+        ];
+        var Gauge_layout = { 
+            width: 700, 
+            height: 600, 
+            margin: { t: 20, b: 40, l:100, r:100 } 
+          };
+        chartsly.newcharts("gauge", Guage_data, Gauge_layout);
+      });
+  }  
+
+function init() {
+    //read the data
+    d3.json("samples.json").then((data)=> {
+        //console.log(data);
+
+        //get the name id to the dropdown menu
+        data.names.forEach((name) => {
+            d3.select("#selDataset")
+            .append("option")
+            .text(name)
+            .property("value");
+        });
+        charts(data.names[0]);
+        fetchVariables(data.names[0]);
+    });
+};
+init();
+
+//change event function
+function optionChanged(id){
+    charts(id);
+    fetchVariables(id);
+};
