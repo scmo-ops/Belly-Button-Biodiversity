a49b8bab02701916f6582ea6c1e64572fee6579b
scmo-ops <a01610125@exatec.tec.mx>
2021-08-15 21:38:38 -0500

    final changes

--- static/app.js
+++ static/app.js
@@ -1,81 +1,69 @@
-// Fetching the variable IDS from the big json file
-
+// Guess I'll start by fetching the variables
 
 function fetchVariables(id){
+
     d3.json("samples.json").then((data)=> {
         var metadata = data.metadata;
-        console.log(metadata)
+        console.log(metadata);
 
-        // Filtering the data by ID
+        //filter demo info data by id
+        var filterResult = metadata.filter(info => info.id.toString() === id)[0];
 
-        var resultsFilter = metadata.filter(info => info.id.toString() === id)[0];
+        var panelBody = d3.select("#sample-metadata");
 
-        // insert in html
+        //empty the demo info panel each time before getting new data
+        panelBody.html("");
 
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
-        Object.entries(resultsFilter).forEach((key)=>{
-            panel.append("p").text(key[0] + ":" + key[1]);
+        Object.entries(filterResult).forEach((key)=>{
+            panelBody.append("p").text(key[0] + ":" + key[1]);
         });
     });
 };
 
-
-
-// This section contains the charts
+/////// This section contains the funtions for the charts ///////
 
 function charts(id) {
-
-    // Connect to the json file.
-
-    d3.json("data/samples.json").then((data)=> {
+    d3.json("samples.json").then((data)=> {
         
-        //filter frecuencies value by id
-        var frecuencies = data.metadata.filter(f => f.id.toString() === id)[0];
-        frecuencies = frecuencies.frecuencies;
-        console.log("Washing Freq: " + frecuencies);
-        
-        // filter samples+ values by id 
+        var freqw_value = data.metadata.filter(f => f.id.toString() === id)[0];
+        freqw_value = freqw_value.wfreq;
+        console.log("Freq washing: " + freqw_value); 
         var samples = data.samples.filter(s => s.id.toString() === id)[0];
         
   
-        // Top 10 samples and top 10 with the OTU id's
-        var sampleValue = samples.sample_values.slice(0, 10).reverse();
-        console.log("Best samples: " + sampleValue);
-  
+        // Extracting the top 10 sample values
+
+        var S_values = samples.sample_values.slice(0, 10).reverse();
+        console.log("Top 10 samples: " + S_values);
         var OTU = (samples.otu_ids.slice(0, 10)).reverse();
-        
         var OTU_id = OTU.map(d => "OTU " + d)
   
-        // get the top 10 labels for the charts and reversing it.
+        // Top 10 samples for the plot.
+
         var labels = samples.otu_labels.slice(0, 10).reverse();
         console.log("labels: " + labels);
   
-        // Trace variables
+        // create trace variable for the plot
         var trace = {
-            x: sampleValue,
+            x: S_values,
             y: OTU_id,
             text: labels,
             marker: {
-              color: 'Blue'},
+                color: 'rgb(55, 128, 191)'},
             type:"bar",
             orientation: "h",
         };
   
-        // Join the data to the trace variables
+        // create data variable
         var data = [trace];
-
-        ////// Section for the charts //////
   
   
-        // Bar chat
-
-        //// I decided to use chartsly since it proves to be very easy to use and very effective ////
-
-        chartsly.newcharts("bar", data);
+        // Barchart
+        
+        Plotly.newPlot("bar", data);
       
         // The bubble chart
-        var New_trace = {
+        var bubble_trace = {
             x: samples.otu_ids,
             y: samples.sample_values,
             mode: "markers",
@@ -87,21 +75,24 @@ function charts(id) {
   
         };
   
-        var bubble_layout = {
+        // set the layout for the bubble plot
+        var bubble_form = {
             xaxis:{title: "OTU ID"},
             height: 600,
             width: 1200
         };
   
-        var buble_data = [New_trace];
+        // creating data variable 
+        var data_bubble = [bubble_trace];
   
-        chartsly.newcharts("bubble", buble_data, bubble_layout); 
+        // create the bubble plot
+        Plotly.newPlot("bubble", data_bubble, bubble_form); 
   
-        // Guage chart
-        var Guage_data = [
+        // Gauge chart
+        var gauge_data = [
           {
           domain: { x: [0, 1], y: [0, 1] },
-          value: frecuencies,
+          value: freqw_value,
           title: {text: `Belly Button Washing Frequency`},
           type: "indicator",
           
@@ -110,31 +101,32 @@ function charts(id) {
                    steps: [
                     {range: [0, 1], color: "white"},
                     {range: [1, 2], color: "white"},
-                    {range: [2, 3], color: "white"},
-                    {range: [3, 4], color: "white"},
-                    {range: [4, 5], color: "white"},
-                    {range: [5, 6], color: "white"},
-                    {range: [6, 7], color: "white"},
-                    {range: [7, 8], color: "white"},
-                    {range: [8, 9], color: "white"}
+                    {range: [2, 3], color: "yellow"},
+                    {range: [3, 4], color: "yellow"},
+                    {range: [4, 5], color: "orange"},
+                    {range: [5, 6], color: "orange"},
+                    {range: [6, 7], color: "red"},
+                    {range: [7, 8], color: "red"},
+                    {range: [8, 9], color: "black"}
                   ]}
               
           }
         ];
-        var Gauge_layout = { 
+        var gauge_form = { 
             width: 700, 
             height: 600, 
             margin: { t: 20, b: 40, l:100, r:100 } 
           };
-        chartsly.newcharts("gauge", Guage_data, Gauge_layout);
+        Plotly.newPlot("gauge", gauge_data, gauge_form);
       });
   }  
 
+  // funtion to get dataset for the drowdown
+
 function init() {
-    //read the data
     d3.json("samples.json").then((data)=> {
 
-        //get the name id to the dropdown menu
+        //dropdown menu
         data.names.forEach((name) => {
             d3.select("#selDataset")
             .append("option")
@@ -147,7 +139,7 @@ function init() {
 };
 init();
 
-//change event function
+//Event funtion
 function optionChanged(id){
     charts(id);
     fetchVariables(id);
